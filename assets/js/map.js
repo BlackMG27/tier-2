0323aa82c9775b4cf3ef0b306b897476d01c27cc
BlackMG27 <oike279@gmail.com>
2019-08-28 16:23:49 -0400

    got the markers to show

--- assets/js/map.js
+++ assets/js/map.js
@@ -16,100 +16,99 @@ const map = new mapboxgl.Map({
     zoom: 13.5
 });
 
-map.on('load', function () {
-    map.addLayer({
-        "id": 'places',
-        "type": 'symbol',
-        "source": {
-            "type": "geojson",
-            "data": {
-                "type": "FeatureCollection",
-                "features": [
-                    {
-                        "type": "Feature",
-                        "properties": {
-                            "description": `<h2>The Field Museum</h2>`,
-                            'marker-color': '#3bb2d0',
-                            'marker-size': 'large',
-                            'marker-symbol': 'rocket'
-                        },
+const geojson = {
+    type: 'FeatureCollection',
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "title": `The Field Museum`,
+                "description": `https://https://www.fieldmuseum.org/`
+            },
 
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [-87.61694, 41.86611]
-                        }
-                    }, {
-                        "type": "Feature",
-                        "properties": {
-                            "description": `<h2>Shedd Aquarium</h2>`,
-                            'marker-color': '#3bb2d0',
-                            'marker-size': 'large',
-                            'marker-symbol': 'rocket'
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [-87.613889, 41.867778]
-                        }
-                    }, {
-                        "type": "Feature",
-                        "properties": {
-                            "description": `<h2>Adler Planetarium</h2>`,
-                            'marker-color': '#3bb2d0',
-                            'marker-size': 'large',
-                            'marker-symbol': 'rocket'
-                        },
-                        "geometry": {
-                            "type": "Point",
-                            "coordinates": [-87.606783, 41.866333]
-                        }
-                    }
-                ]
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-87.61694, 41.86611]
+            }
+        }, {
+            "type": "Feature",
+            "properties": {
+                "title": `Shedd Aquarium`,
+                "description": `https://www.sheddaquarium.org/`
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-87.613889, 41.867778]
+            }
+        }, {
+            "type": "Feature",
+            "properties": {
+                "title": `Adler Planetarium`,
+                "description": `https://www.adlerplanetarium.org/`
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-87.606783, 41.866333]
             }
         }
-    })
+    ]
+};
 
-    const popup = new mapboxgl.Popup({closeButton: false, closeOnClick: false});
+function showMarkers() {
+    geojson
+        .features
+        .forEach((marker) => {
+            const mark = document.createElement('div');
+            mark.className = 'marker';
 
-    map.on('mouseenter', 'places', function (e) {
-        map
-            .getCanvas()
-            .style
-            .cursor = 'pointer';
-        const coordinates = e
-            .features[0]
-            .coordinates
-            .slice();
-        const description = e.features[0].properties.description;
+            new mapboxgl
+                .Marker(marker)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(new mapboxgl.Popup({offset: 25})
+                // add popups
+                    .setHTML(` <h2>${marker.properties.title}</h2> <a href='${marker.properties.description}'>Learn More</a>`))
+                .addTo(map);
+        });
+}
 
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0]
-                ? 360
-                : -360;
-        }
+//gets features to show
+function showTitles() {
+    geojson
+        .features
+        .forEach((place) => {
+            const location = document.createElement('h3');
+            location.className = 'place';
+            location.textContent = place.properties.title;
+            places.appendChild(location)
+        })
+}
 
-        popup
-            .setLngLat(coordinates)
-            .setHTML(description)
-            .addTo(map);
-
-    });
-    map.on('mouseleave', 'places', function () {
-        map
-            .getCanvas()
-            .style
-            .cursor = '';
-        popup.remove();
-    });
-});
-
+showMarkers();
+showTitles();
 //filters the places
-filterText.addEventListener('keyup', function (e) {
-    e.preventDefault()
-    let text = filterText.value;
-    console.log(text);
+filterText.addEventListener('keyup', () => {
+    let text = filterText
+        .value
+        .toLowerCase();
+    //check to see if the value is in the title
+
+    removeMarkers(text)
 })
 
 //get the ids
 function getID(id) {
     return document.getElementById(id);
+}
+//filter the results
+function removeMarkers(text) {
+    //make the number
+    geojson
+        .features
+        .filter((marker) => checkMarker);
+}
+
+function checkMarker() {
+    if (!marker.properties.title.includes(text)) {
+        marker.remove();
+    }
 }
